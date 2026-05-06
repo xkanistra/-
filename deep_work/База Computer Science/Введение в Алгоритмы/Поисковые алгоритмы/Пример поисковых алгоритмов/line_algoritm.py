db5745a01daa5b6ace3219b9cf95568d7058dad7
Kirill <kirya.garkusha.92@gmail.com>
2026-05-06 14:57:47 +0300

    feat: Прохожу поисковые алгоритмы

---
+++ deep_work/База Computer Science/Введение в Алгоритмы/Поисковые алгоритмы/Пример поисковых алгоритмов/line_algoritm.py
@@ -0,0 +1,17 @@
+# Пример линейного поиска
+def main():
+    a_list = [1, 52, 66, 33, 69, 51, 55, 420, 52, 30]
+    n = 51
+    search = linear_search(a_list, n)
+    print(search)
+def linear_search(a_list, n):
+    for i in a_list:
+        if i == n:
+            return True
+        else:
+            return False
+    
+    
+
+if __name__ == '__main__':
+    main()
