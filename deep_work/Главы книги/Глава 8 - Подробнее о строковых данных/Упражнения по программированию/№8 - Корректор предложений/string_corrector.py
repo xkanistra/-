9e63a6549885441b4c02c6bbfdeabb2fb1e4b365
Kiril Garkusha <kirya.garkusha.92@gmail.com>
2026-05-30 12:28:20 +0300

    feat: доделал 8 задание

--- deep_work/Главы книги/Глава 8 - Подробнее о строковых данных/Упражнения по программированию/№8 - Корректор предложений/string_corrector.py
+++ deep_work/Главы книги/Глава 8 - Подробнее о строковых данных/Упражнения по программированию/№8 - Корректор предложений/string_corrector.py
@@ -1,13 +1,11 @@
 # Программа корректирует текст делая буквы верхним регистром где это необходимо
 import re
 
-DATA_FILE = "Главы книги/Глава 8 - Подробнее о строковых данных/Упражнения по программированию/Условия для задач/text.txt"
-
 
 def main():
     user_text = input_text()
-    corrector = corrector_str(user_text)
-
+    result = corrector_str(user_text)
+    print(result)
 
 def input_text():
     text = input("Введите ваше сообщение: ")
@@ -16,9 +14,12 @@ def input_text():
 
 def corrector_str(usr_text):
     corrector = []
+    finaly = []
     raw_text = re.split(r"[.!?]", usr_text)
     text = [s.strip() for s in raw_text if s.strip()]
-
+    for group in text:
+        corrector.append(group.upper()[:1] + group[1:] + '.')
+    return ' '.join(corrector)
 
 
 if __name__ == "__main__":
